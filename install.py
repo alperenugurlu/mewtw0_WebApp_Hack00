da0cc8269ceb8a04760655b8c45138ed715f4cd9
Alperen Ugurlu <64872731+alperenugurlu@users.noreply.github.com>
2022-03-27 12:59:41 +0300

    Create install.py

---
+++ install.py
@@ -0,0 +1,24 @@
+import os
+nmap = os.system('apt-get' + ' ' + 'install' + '' + 'nmap')
+print(nmap)
+
+dirb = os.system('apt' + ' ' + 'install' + '' + 'dirb')
+print(dirb)
+
+gobuster = os.system('apt' + ' ' + 'install' + '' + 'gobuster')
+print(gobuster)
+
+theharvester = os.system('apt' + ' ' + 'install' + '' + 'theharvester')
+print(theharvester)
+
+dirsearch = os.system('apt' + ' ' + 'install' + '' + 'dirsearch')
+print(dirsearch)
+
+metagoofil = os.system('apt' + ' ' + 'install' + '' + 'metagoofil')
+print(metagoofil)
+
+subfinder = os.system('apt' + ' ' + 'install' + '' + 'subfinder')
+print(subfinder)
+
+whois = os.system('apt' + ' ' + 'install' + '' + 'whois')
+print(whois)
